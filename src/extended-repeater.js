242968402ebdd20d2f2ece4d06b20243624c69f1
Pabizolka <1232312009qq@gmail.com>
2021-02-23 00:38:18 +0300

    feat: add solutions for vigenere-cipher and extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options == undefined) {
+    options = {};
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+  if (options.addition === undefined) {
+    options.addition = "";
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = "|";
+  }
+  let segment = new Array(options.additionRepeatTimes).fill(
+    String(options.addition)
+  );
+  let result = new Array(options.repeatTimes).fill(
+    String(str) + segment.join(String(options.additionSeparator))
+  );
+  return result.join(options.separator);
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,81 @@
 const CustomError = require("../extensions/custom-error");
+const ALPHABET = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+
+  encrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let resultMessageArr = [];
+    let count = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (!ALPHABET.includes(message[i])) {
+        resultMessageArr.push(message[i]);
+        continue;
+      }
+      resultMessageArr.push(
+        ALPHABET[
+          (ALPHABET.indexOf(message[i]) + ALPHABET.indexOf(key[count])) % 26
+        ]
+      );
+      count = count < key.length - 1 ? count + 1 : 0;
+    }
+    return this.isDirect
+      ? resultMessageArr.join("")
+      : resultMessageArr.reverse().join("");
+  }
+
+  decrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let resultMessageArr = [];
+    let count = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (!ALPHABET.includes(message[i])) {
+        resultMessageArr.push(message[i]);
+        continue;
+      }
+      resultMessageArr.push(
+        ALPHABET[
+          (ALPHABET.indexOf(message[i]) + 26 - ALPHABET.indexOf(key[count])) %
+            26
+        ]
+      );
+      count = count < key.length - 1 ? count + 1 : 0;
+    }
+    return this.isDirect
+      ? resultMessageArr.join("")
+      : resultMessageArr.reverse().join("");
   }
 }
 
